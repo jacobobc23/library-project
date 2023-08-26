b3d7cc85fe73530fc3d11ccff31cb7bf95fd9681
Johan0425 <114786001+Johan0425@users.noreply.github.com>
2023-08-25 22:17:48 -0500

    Se inicializa la ventana desde la clase main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import view.logIn.LogInWindow;
+
 /**
  *
  * @author Jacobo-bc
@@ -11,6 +13,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> {
+            new LogInWindow().setVisible(true);
+        });
     }
-    
+
 }
